72434b2c332a1cc1afc31b9b8a2e049dab8541b0
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-04-09 20:50:35 -0300

    feat: add middlware

---
+++ internal/infra/middleware/ensure-authenticated.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Missing authentication token"})
+
+			c.Abort()
+			return
+		}
+
+		authKey := os.Getenv("AUTH_KEY")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(authKey), nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		if !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
