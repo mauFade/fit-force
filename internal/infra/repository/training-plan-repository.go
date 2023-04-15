bc4703531b3d0f033bf5f35a20bcaf061e8e71aa
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-04-15 17:52:29 -0300

    feat: sql queries

--- internal/infra/repository/training-plan-repository.go
+++ internal/infra/repository/training-plan-repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/mauFade/fit-force/internal/model"
+	"gorm.io/gorm"
+)
 
 type TrainingPlanDatabaseRepository struct {
 	DB *gorm.DB
@@ -10,4 +13,16 @@ func NewTrainigPlanRepository(database *gorm.DB) *TrainingPlanDatabaseRepository
 	return &TrainingPlanDatabaseRepository{
 		DB: database,
 	}
+}
+
+func (repo *TrainingPlanDatabaseRepository) Create(training_plan *model.TrainingPlan) {
+	repo.DB.Create(training_plan)
+}
+
+func (repo *TrainingPlanDatabaseRepository) Find() []*model.TrainingPlan {
+	var trainings []*model.TrainingPlan
+
+	repo.DB.Raw("SELECT * FROM training_plans").Scan(&trainings)
+
+	return trainings
 }
