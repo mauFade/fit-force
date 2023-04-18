3e9ebe5e902f05d424be3c027e71b31aaa5a36e1
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-04-18 17:17:23 -0300

    feat: create plan use case

--- internal/usecase/training-plan/create-training-plan-usecase.go
+++ internal/usecase/training-plan/create-training-plan-usecase.go
@@ -1,6 +1,31 @@
 package usecase
 
-import "github.com/mauFade/fit-force/internal/infra/repository"
+import (
+	"time"
+
+	"github.com/mauFade/fit-force/internal/infra/repository"
+	"github.com/mauFade/fit-force/internal/model"
+)
+
+type CreateTrainingPLanInputDTO struct {
+	UserID      string
+	WorkoutID   string
+	Name        string
+	Description string
+	Objective   string
+	Duration    string
+}
+
+type CreateTrainingPLanOutputDTO struct {
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	WorkoutID   string    `json:"workout_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Objective   string    `json:"objective"`
+	Duration    string    `json:"duration"`
+	CreatedAt   time.Time `json:"created_at"`
+}
 
 type CreateTrainingPlanUseCase struct {
 	TrainingRepository repository.TrainingPlanDatabaseRepository
@@ -10,4 +35,34 @@ func NewCreateTrainingPlanUseCase(repo repository.TrainingPlanDatabaseRepository
 	return &CreateTrainingPlanUseCase{
 		TrainingRepository: repo,
 	}
+}
+
+func (use_case *CreateTrainingPlanUseCase) Execute(data CreateTrainingPLanInputDTO) (*CreateTrainingPLanOutputDTO, error) {
+	training_plan := model.NewTrainingPLan(
+		data.Name,
+		data.UserID,
+		data.WorkoutID,
+		data.Description,
+		data.Objective,
+		data.Duration,
+	)
+
+	err := training_plan.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+
+	use_case.TrainingRepository.Create(training_plan)
+
+	return &CreateTrainingPLanOutputDTO{
+		ID:          training_plan.ID,
+		UserID:      training_plan.UserID,
+		WorkoutID:   training_plan.WorkoutID,
+		Name:        training_plan.Name,
+		Description: training_plan.Description,
+		Objective:   training_plan.Objective,
+		Duration:    training_plan.Duration,
+		CreatedAt:   training_plan.CreatedAt,
+	}, nil
 }
